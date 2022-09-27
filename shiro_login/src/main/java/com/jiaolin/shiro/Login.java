ec5040048c47ab206a8ced226e1f8060ccacc44e
JionLin <250912986@qq.com>
2022-09-27 19:53:10 +0800

    init commit 明天抽时间继续操作。

---
+++ shiro_login/src/main/java/com/jiaolin/shiro/Login.java
@@ -0,0 +1,14 @@
+package com.jiaolin.shiro;
+
+/**
+ * @author johnny 目的是 用户登录 获取角色  获取权限
+ * @Classname Login
+ * @Description
+ * @Date 2022/9/27 18:39
+ */
+public class Login {
+    public static void main(String[] args) {
+
+    }
+
+}
